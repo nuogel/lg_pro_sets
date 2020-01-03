da17fc0c4b7cb6c2bf25851b2a952abb1856e065
luogeng <990493860@qq.com>
2020-01-03 17:30:40 +0800

    debug the data aug when no labels

--- util/util_is_use_cuda.py
+++ util/util_is_use_cuda.py
@@ -5,4 +5,6 @@ def _is_use_cuda(GPU_NUM=0):
     use_cuda = False
     if GPU_NUM is not -1 and torch.cuda.is_available():
         use_cuda = True
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     return use_cuda
