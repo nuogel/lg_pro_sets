890ad6d64c4b89298c8f4ea39ba54cf10cedff03
luogeng <luogeng@.querycap.com>
2021-08-02 18:40:48 +0800

    try torch2trt

--- torch2tensorrt/torch2tensorrt.py
+++ torch2tensorrt/torch2tensorrt.py
@@ -1,24 +1,38 @@
 import tensorrt
 import torch
-import onnx
-import onnxruntime as rt
-
+# import onnx
+# import onnxruntime as rt
+import cv2
 import numpy as np
-import onnx_tensorrt.backend as backend
+# import onnx_tensorrt.backend as backend
 
 print(tensorrt.__version__)
 
+def get_img_np_nchw(filename):
+    image = cv2.imread(filename)
+    image_cv = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_cv = cv2.resize(image_cv, (640, 640))
+    miu = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229,0.224,0.225])
+    img_np = np.array(image_cv, dtype=float) / 255.
+    r = (img_np[:, :, 0] - miu[0]) / std[0]
+    g = (img_np[:, :, 1] - miu[1]) / std[1]
+    b = (img_np[:, :, 2] - miu[2]) / std[2]
+    img_np_t = np.array([r, g, b])
+    img_np_nchw = np.expand_dims(img_np_t, axis=0)
+    return np.asarray(img_np_nchw,dtype=np.float32)
 
 def torch2onnx():
-    model = './yolov5.pth'
-    dummy_input = torch.randn([1, 3, 640, 640])
+    model = '../tmp/checkpoints/yolov5/now.pkl'
+    filename = '/media/dell/data/voc/VOCdevkit/VOC2007/trainval/JPEGImages/000005.jpg'
+    imgdata = torch.from_numpy(get_img_np_nchw(filename))
     model = torch.load(model)
-    out = model.forward(dummy_input)
-    torch.onnx.export(model, args=dummy_input, f="yolov5.onnx",
+    out = model.forward(imgdata)
+    torch.onnx.export(model, args=imgdata, f="yolov5.onnx",
                       export_params=True,
                       verbose=True,
-                      input_names=['label'],
-                      output_names=["synthesized"],
+                      input_names=['img'],
+                      output_names=["f1", 'f2', 'f3'],
                       opset_version=11)
 
 
@@ -33,18 +47,21 @@ def onnx2trt_with_code():  # failed
 
 
 def onnx2trt_with_trt():  # failed
-    trt_path='yolov5.trt'
-    onnx_path='yolov5.onnx'
+    trt_path = 'yolov5.trt'
+    onnx_path = 'yolov5.onnx'
 
 
 def onnx_runtime():
-    imgdata = np.randn([1, 3, 640, 640])
-    sess = rt.InferenceSession('test.onnx')
+    # imgdata = np.asarray(np.random.rand(1, 3, 640, 640), dtype=np.float32)
+    filename = '/media/dell/data/voc/VOCdevkit/VOC2007/trainval/JPEGImages/000005.jpg'
+    imgdata = get_img_np_nchw(filename)
+    sess = rt.InferenceSession('yolov5.onnx')
     input_name = sess.get_inputs()[0].name
-    output_name = sess.get_outputs()[0].name
-    pred_onnx = sess.run([output_name], {input_name: imgdata})
+    output_name = [sess.get_outputs()[0].name, sess.get_outputs()[1].name, sess.get_outputs()[2].name]
+    pred_onnx = sess.run(output_name, {input_name: imgdata})
     print("outputs:")
-    print(np.array(pred_onnx))
+
 
 if __name__ == '__main__':
     torch2onnx()
+    onnx_runtime()
