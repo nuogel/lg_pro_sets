5d77872b5926a0199051bb6e6fb5d78a45290d8e
luogeng <luogeng@.querycap.com>
2021-11-23 16:48:14 +0800

    lg

---
+++ lgdet/util/util_PIL_show.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+
+
+pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# pil_img.show()
+# 生成画笔
+draw = ImageDraw.Draw(pil_img)
+# 第一个参数是字体文件的路径，第二个是字体大小
+font = ImageFont.truetype('/home/dell/lg/rock_lg/utils/font/simhei.ttf', 60, encoding='utf-8')
+draw.rectangle(((bbox[0], bbox[1]), (bbox[2], bbox[3])),outline='red',width=4)
+draw.text((bbox[0], bbox[1]-61), '行人',fill='red', font=font)
--- scripts/train_yolov5.sh
+++ scripts/train_yolov5.sh
@@ -1,1 +1,1 @@
-python train.py --type OBD --model yolov5  --cp 0  --bz 24 --nw 8 --ema 1 --autoamp 1
+python train.py --type OBD --model yolov5  --cp 1  --bz 24 --nw 8 --ema 1 --autoamp 1
